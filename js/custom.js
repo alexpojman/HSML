059d83300b05cdf2fb44581974980ce24dc8d995
Alex Pojman <pojman.alex@gmail.com>
2015-03-06 16:39:13 -0600

    Lazy Load, New Nav Bar, Small Changes

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,32 @@
+// Transitions
+$(document).ready(function() {
+  
+  $(".animsition").animsition({
+  
+    inClass               :   'fade-in',
+    outClass              :   'fade-out',
+    inDuration            :    400,
+    outDuration           :    100,
+    linkElement           :   '.animsition-link',
+    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+    loading               :    true,
+    loadingParentElement  :   'main-body', //animsition wrapper element
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ],
+    //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    
+    overlay               :   false,
+    
+    overlayClass          :   'animsition-overlay-slide',
+    overlayParentElement  :   'body'
+  });
+});
 
-// Destory modal after it has been loaded
+// Destroy modal after it has been loaded
 $('body').on('hidden.bs.modal', '.modal', function () {
     $(this).removeData('bs.modal');
 });
@@ -10,7 +37,6 @@ $('#disclaimerModal').on('show.bs.modal', function (e) {
   $('#accept_btn').attr("href", link);
 });
 
-
 //triggered when modal is about to be shown
 $(document).on('click', '#accept_btn', function(e) {
 	document.location.href = $('#accept_btn').attr("href");
@@ -18,4 +44,9 @@ $(document).on('click', '#accept_btn', function(e) {
 
 $(document).on("click", "#printModal", function(event){
     $("#professionalModal-content").printElement({pageTitle:$("#professionalModal-content").find(".modal-title").text()});
+});
+
+// Lazy Load
+$(function() {
+    $("img.lazy").lazyload();
 });
